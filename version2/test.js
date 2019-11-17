4ddc4b03f25c0e755fe96ed158e7e238e3694eab
Ali Dagdeviren <54191107+adagdeviren@users.noreply.github.com>
2019-11-17 11:43:11 +0100

    js3-test-finished-aliD

--- version2/test.js
+++ version2/test.js
@@ -19,16 +19,77 @@ const books = [
 ];
 // - Iterates over the array and outputs the details of each item nto the DOM. 
 // - Displays the items inside an unordered list (created programmatically using JavaScript)
+function displayBookInfo(arr) {
+  const ul = document.createElement('ul');
+  document.body.appendChild(ul);
+
+  arr.forEach(obj => {
+    console.log(obj.bookName, obj.author);
+    const li = document.createElement('li');
+    ul.appendChild(li);
+    const bookName = document.createElement('p');
+    const author = document.createElement('p');
+    const image = document.createElement('img');
+
+    li.appendChild(bookName);
+    li.appendChild(author);
+    li.appendChild(image);
+
+    bookName.textContent = "Book Name: " + obj.bookName;
+    author.textContent = "Author: " + obj.author;
+    image.src = obj.coverURL;
+  });
+
+}
+displayBookInfo(books);
+
+
 
 // 2.
 // Write a function that:
 // - Makes an API call using the Fetch API or the regular XMLHttpRequest
 // - Uses the following URL: https://jsonplaceholder.typicode.com/users/1
 // - Displays the "name", "email" and "city" of the first person inside an unordered list (created programmatically using JavaScript) 
+const url = "https://jsonplaceholder.typicode.com/users/1";
+function fetchData(url) {
+  fetch(url)
+    .then(res => res.json()
+      .then(data => {
+        console.log(data);
+
+        const ul = document.createElement('ul');
+        document.body.appendChild(ul);
+        const name = document.createElement('li');
+        name.textContent = data.name;
+        ul.appendChild(name);
+
+        const email = document.createElement('li');
+        email.textContent = data.email;
+        ul.appendChild(email);
+
+        const city = document.createElement('li');
+        city.textContent = data.address.city;
+        ul.appendChild(city);
+
+      }));
+
+}
+fetchData(url);
+
 
 // 3.
 // Write a function that:
 // - Takes in an array of country names
 // - Returns the country with the longest name. 
 // - Uses the following array as an argument: 
-const countries = ["The Netherlands", "Syria", "Turkey", "Mexico", "United States"].
+const countries = ["The Netherlands", "Syria", "Turkey", "Mexico", "United States"]
+
+function findLongest(arr) {
+  let max = arr[0].length;
+  arr.map(v => max = Math.max(max, v.length));
+  result = arr.filter(v => v.length == max);
+  return result;
+
+}
+
+findLongest(countries);
